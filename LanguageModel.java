9fd98229005064925b2bead1ca915af0bc2dda6b
omerheifetz <omer.heifetz@post.runi.ac.il>
2026-01-14 21:08:38 +0200

    HW9-OMERHEIFETZ-REPAIR1

--- LanguageModel.java
+++ LanguageModel.java
@@ -106,7 +106,7 @@ public class LanguageModel {
         }
         String generatedText = initialText;
         String window = generatedText.substring(generatedText.length() - windowLength);
-        while (generatedText.length() < textLength) {
+        while (generatedText.length() < textLength + initialText.length()) {
             List probs = CharDataMap.get(window);
             if (probs == null) {
                 return generatedText;
